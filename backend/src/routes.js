3a80c2b2ac4e75e97487c09be96f4dba2ce0c574
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-16 00:43:57 -0300

    Adicionando as rotas do DevController.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,10 @@ const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
 routes.get('/devs', DevController.index);
+routes.get('/devs/:id', DevController.show);
 routes.post('/devs', DevController.store);
+routes.put('/devs/:id', DevController.update);
+routes.delete('/devs/:id', DevController.destroy);
 
 routes.get('/search', SearchController.index);
 
