b5834e35ddb56a253ec9246a7e4fc7aa18c16ab3
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-15 23:13:43 -0300

    Aula 02

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+});
+
+app.use(express.json())
+app.use(routes);
+
+app.listen(3003);
